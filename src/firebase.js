bb4cf8c9c1026bca1bf986cc5fe62559b6b073f9
Giovani Abel <giovani@prevision.com.br>
2021-05-25 10:27:23 -0300

    Removed sensible information

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firebase-messaging'
 import 'firebase/firebase-database'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDrnTUQM6uLEqFCk9024LRjFqNli9CoVAk",
-  authDomain: "jamal-notifications.firebaseapp.com",
-  projectId: "jamal-notifications",
-  storageBucket: "jamal-notifications.appspot.com",
-  databaseURL: "https://jamal-notifications-default-rtdb.firebaseio.com",
-  messagingSenderId: "450673548894",
-  appId: "1:450673548894:web:e1e3c7e1aac82a9b1f5ecf"
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  databaseURL: "DATABASE_URL",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
 
 
